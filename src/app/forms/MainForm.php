8415c09dfb57f267dcb5b896d4f061ba6a3dd87a
fl3xice <get.flexice@gmail.com>
2019-09-09 12:24:50 +0400

    clicker maked

--- src/app/forms/MainForm.php
+++ src/app/forms/MainForm.php
@@ -7,4 +7,30 @@ use std, gui, framework, app;
 class MainForm extends AbstractForm
 {
 
+    private $clicks = 0;
+
+    /**
+     * @event show 
+     */
+    function doShow(UXWindowEvent $e = null)
+    {    
+    }
+
+    /**
+     * @event button.click-Left 
+     */
+    function doButtonClickLeft(UXMouseEvent $e = null)
+    {    
+        $this->clicks++;
+        $this->labelAlt->text = $this->clicks;
+    }
+
+    /**
+     * @event rectAlt.click-Left 
+     */
+    function doRectAltClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
 }
